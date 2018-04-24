9997b6b328cb6b18494424a2177d888fac038a3d
Solomon Appier-Sign <sappiah@payswitch.com.gh>
2018-04-24 10:25:57 +0000

    wallet balance migrated

--- project/database/migrations/2018_04_24_102241_add_wallet_balance_to_users_table.php
+++ project/database/migrations/2018_04_24_102241_add_wallet_balance_to_users_table.php
@@ -14,7 +14,7 @@ class AddWalletBalanceToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('wallet_balance')->after('wallet_id')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddWalletBalanceToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('wallet_balance');
         });
     }
 }
