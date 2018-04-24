23fc2f434866b407994c876a3a6940d289c5bb86
Solomon Appier-Sign <sappiah@payswitch.com.gh>
2018-04-24 10:27:45 +0000

    balance controller generated

---
+++ project/app/Http/Controllers/BalanceController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 4/24/2018
+ * Time: 10:27 AM
+ */
+
+namespace App\Http\Controllers;
+
+
+class BalanceController extends Controller
+{
+    
+}
